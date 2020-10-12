7faaf5a1851c4592d266a4392ebb7431806c3fe3
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-10-12 11:17:24 +0100

    test

--- src/pages/HomePage/Helpers.js
+++ src/pages/HomePage/Helpers.js
@@ -1,24 +1,28 @@
-require('dotenv').config()
-const axios = require('axios').default;
-export function GlobalFetch(channelId) {
-/** Needed to make Youtube call to grab Videos
- * ChannelID & API Key & Results requested #
- */
- const API = process.env.GOOGLE_API
- const URL = 'https://www.googleapis.com/youtube/v3/search?key=' + API + '&channelId=' + channelId + '&part=snippet,id&order=date&maxResults=1'
+import playlistChannels from "./PlaylistData/PlaylistChannels.js";
+require("dotenv").config();
+const axios = require("axios").default;
+export function GlobalFetch(playlistChannels) {
+  /** Needed to make Youtube call to grab Videos
+   * ChannelID & API Key & Results requested #
+   */
+  const API = process.env.GOOGLE_API;
+  const URL =
+    "https://www.googleapis.com/youtube/v3/search?key=" +
+    API +
+    "&channelId=" +
+    playlistChannels.channelId +
+    "&part=snippet,id&order=date&maxResults=15";
 
- /** Do Fetch/Axios Here */
- return axios.get(URL)
-    .then(function (response) { 
-        //handle success
-        console.log(response);
+  /** Do Fetch/Axios Here */
+  return axios
+    .get(URL)
+    .then(function (response) {
+      //handle success
+      console.log(response);
     })
     .catch(function (error) {
-        //handle error
-        console.log(error);
-    })
- /** Return Videos */
-
+      //handle error
+      console.log(error);
+    });
+  /** Return Videos */
 }
-
-
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,92 +1,84 @@
-import React from "react";
-import "./HomePageStyles.css";
-import MuayTiesVideos from "./MuayTiesComponent/MuayTiesComponent.js";
-import ONEFCVideos from "./ONEFCComponent/ONEFCComponent.js";
-import YokkaoVideos from "./YokkaoComponent/YokkaoComponent.js";
-
-export default function HomePage() {
-  return (
-    <div className="HomePageContainer">
-      <MuayTiesVideos />
-      <ONEFCVideos />
-      <YokkaoVideos />
-    </div>
-  );
-}
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import {
-//   CarouselProvider,
-//   Slider,
-//   ButtonBack,
-//   ButtonNext,
-// } from "pure-react-carousel";
-// import "pure-react-carousel/dist/react-carousel.es.css";
+// import React from "react";
 // import "./HomePageStyles.css";
-// import arrowLogo from "../../images/arrowSVG.svg";
-// import playlistChannel from "./PlaylistData/PlaylistChannels.js";
+// import MuayTiesVideos from "./MuayTiesComponent/MuayTiesComponent.js";
+// import ONEFCVideos from "./ONEFCComponent/ONEFCComponent.js";
+// import YokkaoVideos from "./YokkaoComponent/YokkaoComponent.js";
 
-// const API = "AIzaSyDp5XB1HNLomcylZ7CTVR6JxiO07mRpZvk";
-// const result = 1;
-
-// const URLs = playlistChannel.map(
-//   ({ channelId }) =>
-//     `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${result}`
-// );
-
-// export default function MuayTiesComponent() {
-//   const [fights, setFights] = useState([]);
-//   const [titles, setTitles] = useState([]);
-//   useEffect(() => {
-//     URLs.map((URL) => {
-//       return fetch(URL)
-//         .then((response) => response.json())
-//         .then((data) => {
-//           const fightInfo = data.items.map(
-//             ({ id: { videoId } }) => `https://www.youtube.com/embed/${videoId}`
-//           );
-//           const fightTitles = data.items.map(({ snippet: { title } }) => title);
-//           setFights(fightInfo);
-//           setTitles(fightTitles);
-//         });
-//     });
-//   }, []);
-
+// export default function HomePage() {
 //   return (
-//     <div className="videoPageContainer">
-//       {playlistChannel.map((channel) => (
-//         <>
-//           <Link to={channel.pageURL} className="channelTitle">
-//             {channel.channelTitle}
-//           </Link>
-//           <CarouselProvider totalSlides={3} className="videoCarousel">
-//             <Slider>
-//               <div className="vidContainer">
-//                 {fights.map((fight, i) => {
-//                   return (
-//                     <div className="fightInfo">
-//                       <iframe
-//                         className="vidStyles"
-//                         key={i}
-//                         src={fight}
-//                         frameborder="0"
-//                         allowFullScreen
-//                       />
-//                       <div className="fightTitle">{titles}</div>
-//                     </div>
-//                   );
-//                 })}
-//               </div>
-//             </Slider>
-//             <ButtonBack className="backButton">
-//               <img src={arrowLogo} className="backButtonArrow" />
-//             </ButtonBack>
-//             <ButtonNext className="nextButton">
-//               <img src={arrowLogo} />
-//             </ButtonNext>
-//           </CarouselProvider>
-//         </>
-//       ))}
+//     <div className="HomePageContainer">
+//       <MuayTiesVideos />
+//       <ONEFCVideos />
+//       <YokkaoVideos />
 //     </div>
 //   );
 // }
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  CarouselProvider,
+  Slider,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "./HomePageStyles.css";
+import arrowLogo from "../../images/arrowSVG.svg";
+import GlobalFetch from "./Helpers.js";
+import playlistChannels from "./PlaylistData/PlaylistChannels.js";
+
+export default function MuayTiesComponent() {
+  const [fights, setFights] = useState([]);
+  const [titles, setTitles] = useState([]);
+  useEffect(() => {
+    fetch()
+      .then((response) => response.json())
+      .then((data) => {
+        const fightInfo = data.items.map(
+          ({ id: { videoId } }) => `https://www.youtube.com/embed/${videoId}`
+        );
+        const fightTitles = data.items.map(({ snippet: { title } }) => title);
+        setFights(fightInfo);
+        setTitles(fightTitles);
+      });
+  }, []);
+
+  return (
+    <div className="videoPageContainer">
+      {playlistChannels.map(({ channelTitle, pageURL }) => (
+        <>
+          <Link to={pageURL} className="channelTitle">
+            {channelTitle}
+          </Link>
+          <CarouselProvider totalSlides={3} className="videoCarousel">
+            <Slider>
+              <div className="vidContainer">
+                {fights.map((fight, i) => {
+                  return (
+                    <div className="fightInfo">
+                      <iframe
+                        className="vidStyles"
+                        key={i}
+                        src={fight}
+                        frameborder="0"
+                        allowFullScreen
+                      />
+                      <div className="fightTitle">{titles}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </Slider>
+            <ButtonBack className="backButton">
+              <img src={arrowLogo} className="backButtonArrow" />
+            </ButtonBack>
+            <ButtonNext className="nextButton">
+              <img src={arrowLogo} />
+            </ButtonNext>
+          </CarouselProvider>
+        </>
+      ))}
+    </div>
+  );
+}
